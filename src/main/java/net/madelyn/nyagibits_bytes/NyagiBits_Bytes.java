9d2ff6dc13253cf061cddc5fb53184d65d739089
Madelyn Byte <nyagiredspice@gmail.com>
2023-07-11 12:31:42 -0400

    Initial commit
    creating mod jar

---
+++ src/main/java/net/madelyn/nyagibits_bytes/NyagiBits_Bytes.java
@@ -0,0 +1,36 @@
+package net.madelyn.nyagibits_bytes;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(NyagiBits_Bytes.MOD_ID)
+public class NyagiBits_Bytes {
+    public static final String MOD_ID = "nyagibits_bytes";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public NyagiBits_Bytes() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+        }
+    }
+}
