6b765ad9a9390bb6488c30a11643a296da3c155f
Madelyn Byte <nyagiredspice@gmail.com>
2023-07-18 16:37:06 -0400

    Pile Of Cogs & Author Credit

--- src/main/java/net/madelyn/nyagibits_bytes/item/ModItems.java
+++ src/main/java/net/madelyn/nyagibits_bytes/item/ModItems.java
@@ -17,7 +17,7 @@ public class ModItems {
     Flooter: []
 
     Texture authors:
-    SoulRacer: [Livisite_Alloy, Crude Abrasive]
+    SoulRacer: [Livisite_Alloy, Crude Abrasive, Pile of Cogs]
     ---------------------------------------------------------------------------------------*/
 
     //Textures are present in the path : resources > assets > nyagibits_byte > models > item
