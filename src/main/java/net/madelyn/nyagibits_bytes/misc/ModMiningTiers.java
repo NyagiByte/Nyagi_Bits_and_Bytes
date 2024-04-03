852febec16f39ce80f6f73cbd9fb7a55352b3ca3
Killerqu00 <killerqueen1777@gmail.com>
2024-04-03 23:23:39 +0200

    !!!TEMPORARY SOLUTION!!!

---
+++ src/main/java/net/madelyn/nyagibits_bytes/misc/ModMiningTiers.java
@@ -0,0 +1,58 @@
+package net.madelyn.nyagibits_bytes.misc;
+
+import net.madelyn.nyagibits_bytes.NyagiBits_Bytes;
+import net.madelyn.nyagibits_bytes.item.ModItems;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.Tiers;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraftforge.common.ForgeTier;
+import net.minecraftforge.common.TierSortingRegistry;
+
+import java.util.List;
+
+public class ModMiningTiers {
+    public static Tier MANASTEEL;
+    public static Tier ELEMENTIUM;
+    public static Tier TERRASTEEL;
+    public static Tier ALFSTEEL;
+    public static Tier FLAKE;
+
+    static {
+        MANASTEEL = TierSortingRegistry.registerTier(new ForgeTier(5,
+                2500,
+                10.0F,
+                5.0F,
+                20,
+                ModTags.Blocks.NEEDS_MANASTEEL_TOOL,
+                () -> Ingredient.EMPTY), new ResourceLocation(NyagiBits_Bytes.MOD_ID, "manasteel"), List.of(Tiers.NETHERITE), List.of());
+        ELEMENTIUM = TierSortingRegistry.registerTier(new ForgeTier(6,
+                2700,
+                11.0F,
+                6.0F,
+                20,
+                ModTags.Blocks.NEEDS_ELEMENTIUM_TOOL,
+                () -> Ingredient.EMPTY), new ResourceLocation(NyagiBits_Bytes.MOD_ID, "elementium"), List.of(MANASTEEL), List.of());
+        TERRASTEEL = TierSortingRegistry.registerTier(new ForgeTier(7,
+                2800,
+                12.0F,
+                7.0F,
+                20,
+                ModTags.Blocks.NEEDS_TERRASTEEL_TOOL,
+                () -> Ingredient.EMPTY), new ResourceLocation(NyagiBits_Bytes.MOD_ID, "terrasteel"), List.of(ELEMENTIUM), List.of());
+        ALFSTEEL = TierSortingRegistry.registerTier(new ForgeTier(8,
+                2850,
+                13.0F,
+                8.0F,
+                20,
+                ModTags.Blocks.NEEDS_ALFSTEEL_TOOL,
+                () -> Ingredient.EMPTY), new ResourceLocation(NyagiBits_Bytes.MOD_ID, "alfsteel"), List.of(TERRASTEEL), List.of());
+        FLAKE = TierSortingRegistry.registerTier(new ForgeTier(9,
+                2880,
+                14.0F,
+                9.0F,
+                20,
+                ModTags.Blocks.NEEDS_FLAKE_TOOL,
+                () -> Ingredient.EMPTY), new ResourceLocation(NyagiBits_Bytes.MOD_ID, "flake"), List.of(ALFSTEEL), List.of());
+    }
+}
---
+++ src/main/java/net/madelyn/nyagibits_bytes/misc/ModTags.java
@@ -0,0 +1,25 @@
+package net.madelyn.nyagibits_bytes.misc;
+
+import net.madelyn.nyagibits_bytes.NyagiBits_Bytes;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.block.Block;
+
+public class ModTags {
+    public static class Blocks {
+        public static final TagKey<Block> NEEDS_MANASTEEL_TOOL = tag("needs_manasteel_tool");
+        public static final TagKey<Block> NEEDS_ELEMENTIUM_TOOL = tag("needs_elementium_tool");
+        public static final TagKey<Block> NEEDS_TERRASTEEL_TOOL = tag("needs_terrasteel_tool");
+        public static final TagKey<Block> NEEDS_ALFSTEEL_TOOL = tag("needs_alfsteel_tool");
+        public static final TagKey<Block> NEEDS_FLAKE_TOOL = tag("needs_flake_tool");
+
+        private static TagKey<Block> tag(String name) {
+            return BlockTags.create(new ResourceLocation(NyagiBits_Bytes.MOD_ID, name));
+        }
+
+        private static TagKey<Block> forgeTag(String name) {
+            return BlockTags.create(new ResourceLocation("forge", name));
+        }
+    }
+}
