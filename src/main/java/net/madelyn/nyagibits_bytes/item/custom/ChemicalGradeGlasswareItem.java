d846ba25df532464d3e029bea51a74cef587c54b
Madelyn Byte <nyagiredspice@gmail.com>
2024-04-07 19:14:41 -0400

    Chemical Grade Glassware (item) added

---
+++ src/main/java/net/madelyn/nyagibits_bytes/item/custom/ChemicalGradeGlasswareItem.java
@@ -0,0 +1,25 @@
+package net.madelyn.nyagibits_bytes.item.custom;
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class ChemicalGradeGlasswareItem extends Item {
+    public ChemicalGradeGlasswareItem(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> components, TooltipFlag flag) {
+        components.add(Component.literal("Strong temperature resistant glass").withStyle(ChatFormatting.GRAY));
+        components.add(Component.literal("is needed to not send shrapnel").withStyle(ChatFormatting.GRAY));
+        components.add(Component.literal("into your face").withStyle(ChatFormatting.GRAY));
+        super.appendHoverText(stack, level, components, flag);
+    }
+}
