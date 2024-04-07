f1ee45dbaaa2be9d03ad23d53324af8268efb97f
Madelyn Byte <nyagiredspice@gmail.com>
2024-04-07 17:07:44 -0400

    Fume Fan (item) added

---
+++ src/main/java/net/madelyn/nyagibits_bytes/item/custom/FumeFanItem.java
@@ -0,0 +1,24 @@
+package net.madelyn.nyagibits_bytes.item.custom;
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class FumeFanItem extends Item {
+    public FumeFanItem(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> components, TooltipFlag flag) {
+        components.add(Component.literal("The atmosphere is nature's garbage").withStyle(ChatFormatting.GRAY));
+        components.add(Component.literal("bin - Explosions & Fire").withStyle(ChatFormatting.GRAY));
+        super.appendHoverText(stack, level, components, flag);
+    }
+}
