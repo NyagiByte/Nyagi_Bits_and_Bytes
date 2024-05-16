836bd8e4d6b889f176710b05ceb06b7dfed8644e
jjfireblast101 <jjfireblast@gmail.com>
2024-05-17 00:54:28 +0100

    Fluid Template Go brrr
    I need sleep...

--- src/main/java/net/madelyn/nyagibits_bytes/fluid/ModFluidTypes.java
+++ src/main/java/net/madelyn/nyagibits_bytes/fluid/ModFluidTypes.java
@@ -3,6 +3,8 @@ package net.madelyn.nyagibits_bytes.fluid;
 import com.mojang.math.Vector3f;
 import net.madelyn.nyagibits_bytes.NyagiBits_Bytes;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraftforge.common.SoundAction;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fluids.FluidType;
 import net.minecraftforge.registries.DeferredRegister;
@@ -15,6 +17,10 @@ public class ModFluidTypes {
     public static final ResourceLocation SOAP_OVERLAY_RL = new ResourceLocation(NyagiBits_Bytes.MOD_ID, "misc/in_soap_water");
 
     public static final DeferredRegister<FluidType> FLUID_TYPES = DeferredRegister.create(ForgeRegistries.Keys.FLUID_TYPES, NyagiBits_Bytes.MOD_ID);
+
+    public static final RegistryObject<FluidType> SOAP_WATER_FLUID_TYPE = register("soap_water_fluid",
+            FluidType.Properties.create().lightLevel(2).density(15).viscosity(5).sound(SoundAction.get("drink"),
+                    SoundEvents.HONEY_DRINK));
 
     public static RegistryObject<FluidType> register(String name, FluidType.Properties properties) {
         return FLUID_TYPES.register(name, () -> new BaseFluidType(WATER_STILL_RL, WATER_FLOWING_RL, SOAP_OVERLAY_RL, 0xA1E038D0, new Vector3f(224f / 255f, 56f / 255f, 208f / 255f), properties));
--- src/main/java/net/madelyn/nyagibits_bytes/fluid/ModFluids.java
+++ src/main/java/net/madelyn/nyagibits_bytes/fluid/ModFluids.java
@@ -1,4 +1,26 @@
 package net.madelyn.nyagibits_bytes.fluid;
 
+import net.madelyn.nyagibits_bytes.NyagiBits_Bytes;
+import net.minecraft.world.level.material.FlowingFluid;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fluids.ForgeFlowingFluid;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
 public class ModFluids {
+    public static final DeferredRegister<Fluid> FLUIDS =
+            DeferredRegister.create(ForgeRegistries.FLUIDS, NyagiBits_Bytes.MOD_ID);
+
+    public static final RegistryObject<FlowingFluid> SOURCE_SOAP_WATER = FLUIDS.register("soap_water_fluid",
+            () -> new ForgeFlowingFluid.Source(ModFluids.SOAP_WATER_FLUID_PROPERTIES));
+    public static final RegistryObject<FlowingFluid> FLOWING_SOAP_WATER = FLUIDS.register("flowing_soap_water",
+            () -> new ForgeFlowingFluid.Flowing(ModFluids.SOAP_WATER_FLUID_PROPERTIES));
+    public static final ForgeFlowingFluid.Properties SOAP_WATER_FLUID_PROPERTIES = new ForgeFlowingFluid.Properties(
+            ModFluidTypes.SOAP_WATER_FLUID_TYPE, SOURCE_SOAP_WATER, FLOWING_SOAP_WATER).slopeFindDistance(2).levelDecreasePerBlock(2).block().bucket();
+
+    public static void register(IEventBus eventBus) {
+        FLUIDS.register(eventBus);
+    }
 }
