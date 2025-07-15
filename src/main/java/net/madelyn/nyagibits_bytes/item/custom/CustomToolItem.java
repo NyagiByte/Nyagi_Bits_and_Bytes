ac9c32c4e31820a54b2ae83c20cfb6f4dab166fa
Big Barza <francesco.b99@hotmail.it>
2025-07-15 15:14:34 +0200

    Make it repairable

--- src/main/java/net/madelyn/nyagibits_bytes/item/custom/CustomToolItem.java
+++ src/main/java/net/madelyn/nyagibits_bytes/item/custom/CustomToolItem.java
@@ -39,10 +39,5 @@ public class CustomToolItem extends CustomTooltipItem{
         return item;
     }
 
-    @Override
-    public boolean isRepairable(ItemStack stack){
-        return false;
-    }
-
 
 }
