71bc1b6d3e61ff2ae589892f95e1e660dbd75cbf
Big Barza <francesco.b99@hotmail.it>
2026-08-04 22:54:30 +0200

    (I am in pain) Fix buckets - Make them actually a bucket in the eyes of forge, because it only treats pure buckets as its own and subclasses need to implement the capability themselves. Extending BucketItem alone does not a proper bucket make.

--- src/main/java/net/madelyn/nyagibits_bytes/item/custom/CustomBucketItem.java
+++ src/main/java/net/madelyn/nyagibits_bytes/item/custom/CustomBucketItem.java
@@ -2,13 +2,17 @@ package net.madelyn.nyagibits_bytes.item.custom;
 
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.resources.language.I18n;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.BucketItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.material.Fluid;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.fluids.capability.wrappers.FluidBucketWrapper;
 import net.minecraftforge.registries.ForgeRegistries;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -27,6 +31,21 @@ public class CustomBucketItem extends BucketItem {
         super(fluid, props);
         this.chemical = chemical;
     }
+
+    /*
+    *ahem*
+    THIS. THIS IS WHAT WAS MISSING. THIS IS WHY THE BUCKETS WOULDN'T FUCKING WORK WITH BASINS AND WHATNOT.
+    BECAUSE FORGE ONLY DOES IT FOR BucketItem AND NOT SUBCLASSES. Now, it does have a reason to do that.
+    BUT IF YOU ARE SEARCHING GITHUB FOR A SOLUTION TO "CUSTOM BUCKET DOES NOT BUCKET RIGHT", THIS IS PROBABLY IT.
+    HEED OUR WARNING AND DO THIS, OR SUFFER LIKE WE DID.
+    Anyway, thanks create team for leaving a comment about this, and thanks GT-Modern team for making me realize what was going on.
+    */
+
+    @Override
+    public @NotNull ICapabilityProvider initCapabilities(@NotNull ItemStack stack, @Nullable CompoundTag nbt) {
+        return new FluidBucketWrapper(stack);
+    }
+
 
     @Override //This is like the one in CustomTooltipItem, but with a secondary fallback tooltip describing the chemical as a whole.
     public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> components, TooltipFlag flag) {
