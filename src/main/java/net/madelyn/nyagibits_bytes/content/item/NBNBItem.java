4700e45634bd27eb36ce4f964d8d8256704d4ef5
Big Barza <francesco.b99@hotmail.it>
2026-08-28 21:35:58 +0200

    You ever forget an entire feature? - Restored the shift function for mineral tooltips

--- src/main/java/net/madelyn/nyagibits_bytes/content/item/NBNBItem.java
+++ src/main/java/net/madelyn/nyagibits_bytes/content/item/NBNBItem.java
@@ -1,6 +1,7 @@
 package net.madelyn.nyagibits_bytes.content.item;
 
 import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.chat.Component;
@@ -48,14 +49,25 @@ public class NBNBItem extends Item {
 
     @Override
     public void appendHoverText(ItemStack stack, @org.jetbrains.annotations.Nullable Level level, List<Component> components, TooltipFlag flag) {
+        String yieldKey = "item." + ForgeRegistries.ITEMS.getResourceKey(this).get().location().toString().replace(':', '.') + ".yield";
         String descKey = "item." + ForgeRegistries.ITEMS.getResourceKey(this).get().location().toString().replace(':', '.') + ".desc";
         String chemDescKey = "chemical.nyagibits_bytes." + chemical+".desc";
         Component desc = null;
-        if(I18n.exists(descKey)) desc = Component.translatable(descKey);
-        else if (traits.contains(ItemTraits.IS_CHEMICAL) && I18n.exists(chemDescKey)) {
-            desc = Component.translatable(chemDescKey);
+        if(traits.contains(ItemTraits.IS_ORE) && I18n.exists(yieldKey) && Screen.hasShiftDown()){
+            desc = Component.translatable(yieldKey);
+            for(String line : desc.getString().split("\n")) components.add(Component.literal(line).withStyle(ChatFormatting.BLUE));
+        } else {
+            if(I18n.exists(descKey)) desc = Component.translatable(descKey);
+            else if (traits.contains(ItemTraits.IS_CHEMICAL) && I18n.exists(chemDescKey)) {
+                desc = Component.translatable(chemDescKey);
+            }
+            if(desc != null) for(String line : desc.getString().split("\n")) components.add(Component.literal(line).withStyle(ChatFormatting.GRAY));
         }
-        if(desc != null) for(String line : desc.getString().split("\n")) components.add(Component.literal(line).withStyle(ChatFormatting.GRAY));
+
+        if(traits.contains(ItemTraits.IS_ORE) && I18n.exists(yieldKey) && !Screen.hasShiftDown()){
+            components.add(Component.literal(" "));
+            components.add(Component.translatable("nyagi_bits_bytes.tooltip.shift_yield_prompt").withStyle(ChatFormatting.YELLOW));
+        }
         super.appendHoverText(stack, level, components, flag);
     }
 
@@ -90,7 +102,7 @@ public class NBNBItem extends Item {
 
     @Override //This makes the item float. Can also have some fiendish fun by using entity.setDeltaMovement()
     public boolean onEntityItemUpdate(ItemStack stack, ItemEntity entity){
-        if(!traits.contains(ItemTraits.LEVITATES)) return false;
+        if(!traits.contains(ItemTraits.LEVITATES)) return false; //NEVER RETURN TRUE HERE
         Level level = entity.level();
         Vec3 pos = entity.position();
         if(level.isClientSide){ //We don't want to spawn particles on the server's side.
